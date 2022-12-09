5c4e700c38d076a02910a70d4c440d6698536a3d
Gagandeep Singh <100983473+neophyte0903@users.noreply.github.com>
2022-12-09 11:23:56 +0530

    qr code project added

---
+++ qrcode_gen/main.py
@@ -0,0 +1,16 @@
+import qrcode
+import image
+qr=qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border= 5
+)
+
+data=input("enter url here:- ")
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img=qr.make_image(fill="black",back_color="white")
+
+img.save("testr.png")
