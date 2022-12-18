e102eac778691ad4defb860fc79acaf2d3c43011
Gagandeep Singh <100983473+neophyte0903@users.noreply.github.com>
2022-12-18 13:56:01 +0530

    calculator project added

---
+++ calculator with gui/main.py
@@ -0,0 +1,54 @@
+from tkinter import Tk,Entry,Button,StringVar
+
+
+class calc:
+    #ui implementation
+    def __init__(self,master):
+        master.title("calculator")
+        master.geometry("357x420+0+0")
+        master.config(bg='#FFF6C3')
+        master.resizable(False,False)
+        Button(width=11,height=4,text='(',relief='flat',bg='#000000',fg='white',command=lambda:self.show('(')).place(x=0,y=50)
+        Button(width=11,height=4,text=')',relief='flat',bg='#000000',fg='white',command=lambda:self.show(')')).place(x=90,y=50)
+        Button(width=11,height=4,text='%',relief='flat',bg='#000000',fg='white',command=lambda:self.show('%')).place(x=180,y=50)
+        Button(width=11,height=4,text='1',relief='flat',bg='#000000',fg='white',command=lambda:self.show(1)).place(x=0,y=125)
+        Button(width=11,height=4,text='2',relief='flat',bg='#000000',fg='white',command=lambda:self.show(2)).place(x=90,y=125)
+        Button(width=11,height=4,text='3',relief='flat',bg='#000000',fg='white',command=lambda:self.show(3)).place(x=180,y=125)
+        Button(width=11,height=4,text='4',relief='flat',bg='#000000',fg='white',command=lambda:self.show(4)).place(x=0,y=200)
+        Button(width=11,height=4,text='5',relief='flat',bg='#000000',fg='white',command=lambda:self.show(5)).place(x=90,y=200)
+        Button(width=11,height=4,text='6',relief='flat',bg='#000000',fg='white',command=lambda:self.show(6)).place(x=180,y=200)
+        Button(width=11,height=4,text='7',relief='flat',bg='#000000',fg='white',command=lambda:self.show(7)).place(x=0,y=275)
+        Button(width=11,height=4,text='8',relief='flat',bg='#000000',fg='white',command=lambda:self.show(8)).place(x=90,y=275)
+        Button(width=11,height=4,text='9',relief='flat',bg='#000000',fg='white',command=lambda:self.show(9)).place(x=180,y=275)
+        Button(width=11,height=4,text='0',relief='flat',bg='#000000',fg='white',command=lambda:self.show(0)).place(x=90,y=350)
+        Button(width=11,height=4,text='.',relief='flat',bg='#000000',fg='white',command=lambda:self.show('.')).place(x=180,y=350)
+        Button(width=11,height=4,text='+',relief='flat',bg='#000000',fg='white',command=lambda:self.show('+')).place(x=270,y=275)
+        Button(width=11,height=4,text='-',relief='flat',bg='#000000',fg='white',command=lambda:self.show('-')).place(x=270,y=200)
+        Button(width=11,height=4,text='/',relief='flat',bg='#000000',fg='white',command=lambda:self.show('/')).place(x=270,y=50)
+        Button(width=11,height=4,text='X',relief='flat',bg='#000000',fg='white',command=lambda:self.show('*')).place(x=270,y=125)
+        Button(width=11,height=4,text='=',relief='flat',bg='#DC5F00',fg='white',command=lambda:self.solve()).place(x=270,y=350)
+        Button(width=11,height=4,text='C',relief='flat',bg='#B2B2B2',fg='black',command=lambda:self.clear()).place(x=0,y=350)
+        # Button(width=11,height=4,text='',relief='flat',bg='white',command=lambda:self.show().place(x=,y=))
+
+        self.equation=StringVar()
+        self.entry_value=''
+        Entry(width=17,bg="#fff",font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
+
+    def show(self,value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value=""
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        result=eval(self.entry_value) #learn this 
+        self.equation.set(result)
+
+
+
+
+root=Tk()
+calculator=calc(root)
+root.mainloop()
