795007ed7a6e0c8f322fafd8e998732856781fb2
gagandeep singh <gagandeep.21gcebaids001@galgotiacollege.edu>
2023-09-05 19:10:17 +0530

    main inverse added

--- numberGuessing/mainInv.py
+++ numberGuessing/mainInv.py
@@ -6,6 +6,19 @@ def computer_guess(x):
     high = x
     feedback = ''
     while feedback != 'c':
-        guess = random.randint(low, high)
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # can also be high both are same
         feedback = input(
-            f"is the guess {guess} too high (h/H) too low (l/L) or correct(c/C)")
+            f"is the guess {guess} too high (h/H) too low (l/L) or correct(c/C)").lower()
+        if feedback == 'h':
+            high = guess-1
+        if feedback == 'l':
+            low = guess+1
+        else:
+            print("invalid input")
+    print("yaay i guessed it correctly")
+
+
+computer_guess(10)
